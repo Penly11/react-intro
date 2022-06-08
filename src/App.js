f227a5c643cc89c704380127ae8e1d643749bb27
Brian Penly <brianpenly@TpT-BrianP.local>
2022-06-08 10:36:56 -0400

    Added goals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  /**
+   * Goals:
+   *  1. Create a dynamically generated list displaying the names, prices, and in stock statuses of the food items below.
+   *  2. Add a search bar and allow the user to filter the list by the name of the item.
+   *  3. Add a dropdown to filter the list by in stock status.
+   *  4. Add a form that allows the user to add an item to the list.
+   *  5. Add a way for a user to delete an item from the list.
+   *
+   * Functionality is more important than styling.
+   */
+
+  const initialFoods = [
+    {
+      name: 'Cheese',
+      price: 3,
+      inStock: true
+    },
+    {
+      name: 'Apple',
+      price: 4,
+      inStock: true
+    },
+    {
+      name: 'Celery',
+      price: 2,
+      inStock: false
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Delete me!!!
+      </p>
     </div>
   );
 }
